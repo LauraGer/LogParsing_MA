0f5d8e28747edfc451cb54d1cd6ccf46c0218f3e
LauraGer <l.gerlach@icloud.com>
2015-08-01 15:24:04 +0200

    NIMM ES SO

--- src/logparsing_ma/cube_parsing.java
+++ src/logparsing_ma/cube_parsing.java
@@ -7,53 +7,140 @@ package logparsing_ma;
 
 import java.io.*;
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import static logparsing_ma.hadoop_parsing.PATTERNS;
 
 /**
  *
  * @author Laura
  */
 class cube_parsing {
+    
+    static String[]     REGEXS      = new String[] {    /*"\"CUBE\",\"(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2},\\d{3}).+\\[time\\].\\[Day Name\\].+(\\d{5}|[0-9]{10}).+\\[time\\].\\[Day Name\\].+(\\d+).+"
+                                                    ,*/   "\"CUBE\",\"(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2},\\d{3}).+\\[partner\\].(\\[partner\\].\\[Url\\]).+\"(\\d+)\".+\"L-JBOSS-.+WITH MEMBER.+\\[Measures\\].(\\[.+\\]).+\\[Day Name\\].+\\[(\\d+)\\].+"
+                                                    ,   "\"CUBE\",\"(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2},\\d{3}).+(\\[program\\].\\[name\\]).+\"(\\d+)\".+\"L-JBOSS-.+WITH MEMBER.+\\[Measures\\].(\\[.+\\]).+\\[Day Name\\].+\\[(\\d+)\\].+"
+                                                    ,   "\"CUBE\",\"(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2},\\d{3}).+\\[program\\].\\[program\\].+(\\[Prog Name\\]).+\"(\\d+)\".+\"L-JBOSS-.+WITH MEMBER.+\\[Measures\\].(\\[.+\\]).+\\[Day Name\\].+\\[(\\d+)\\].+"        
+                                                    ,   "\"CUBE\",\"(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2},\\d{3}).+\\[affiliation\\].\\[affiliation_admedia\\].+\\[admedia key\\].+\"(\\d+)\".+\"L-JBOSS-.+WITH MEMBER.+\\[Measures\\].(\\[.+\\]).+\\[Day Name\\].+\\[(\\d+)\\].+"        
+                                                    ,   "\"CUBE\",\"(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2},\\d{3}).+\\[Day Name\\].+\\[(\\d+)\\].+\\[Day Name\\].+\\[(\\d+)\\].+\"(\\d+)\".+\"L-JBOSS-.+WITH MEMBER.+\\[Measures\\].(\\[.+\\]).+"};//\",\"(\\d+).+\\[Measures\\].(.+).*"};//.\\[User Name\\].&\\[(.\\d+)\\].*"};
+//    "|\\\\[Day Name\\\\]"[affiliation].[affiliation_admedia].[admedia key]
+    static Pattern[]    PATTERNS    = new Pattern[REGEXS.length];
 
+    static {
+            for (int i = 0; i < REGEXS.length; i++){
+                PATTERNS[i] = Pattern.compile(REGEXS[i]);
+            }
+        }
+    
     static void openCube() throws IOException {
-        
+
         PrintWriter output     =   new PrintWriter("/Volumes/HD/X/LOGS/FINAL_cube.csv");
         
         try (BufferedReader cube = new BufferedReader(new FileReader("/Volumes/HD/htdocs/GitHub/log_stuff/log_cube.csv")))
             {
-                String sCurrentLine;
-
+                String sCurrentLine
+                    ,   INPUT_STRING
+                    ,   timestamp      
+                    ,   range_filter_f 
+                    ,   range_filter_t 
+                    ,   term_filter    
+                    ,   group          
+                    ,   sorting  
+                    ,   order
+                    ,   values         
+                    ,   input
+                    ,   check
+                    ;
+                int i;
+                
+                output.println("System;TimeStamp;RangeFilterFrom;RangeFilterTo;TermFilter;Group;Sorting;Order;Values");
+                
                 while ((sCurrentLine = cube.readLine()) != null) {
                     
-                    String  edit;    
-                            
-                            edit    =	sCurrentLine.replace( ",,,", ";;;")
-                                            .replace( ",,", ";;")
-						.replace( "\",\"", ";");
-                    
-                    String[] parts          =   edit.split(";");
-                    
-                    String  timestamp       =   parts[1]
-                        ,   range_filter_f  =   parts[2]
-                        ,   range_filter_t  =   parts[3]
-                        ,   term_filter     =   parts[4]
-                        ,   group           =   parts[5]
-                        ,   sorting         =   parts[7]
-                        ,   values          =   parts[8]
-                        ,   input
-                        ;
+                    INPUT_STRING    =   sCurrentLine;
+                    i = 0;
 
-                    if( (range_filter_f.length()==0) && (range_filter_t.length()==0) ) {
-                         term_filter  = parts[4];
-//                        if(term_filter.matches("*.$[0-9]{8})$.*")) /*[0-9]{8}$.**/
-//                        {
-//                            term_filter  = "CHECK";
-//                        }
-                    }
-////                    
-                    input       =   ("'CUBE';"+timestamp+";"+range_filter_f+";"+range_filter_t+";"+term_filter+";"+group+";"+sorting+";'"+values+"'");
-//                    System.out.println(Arrays.toString(parts));
-//                    System.out.println(term_filter);
+//                        System.out.println(INPUT_STRING);
+                    for (Pattern pattern : PATTERNS) {
+                       
+                        Matcher matcher = pattern.matcher(INPUT_STRING);
+//                        System.out.println(matcher.matches());
+                        if (matcher.matches()){
+                            i = 1;
+                            timestamp       =   matcher.group(1);
+                            
+//                            if(matcher.group(3).length() ==0){
+//                                range_filter_f  =   matcher.group(2);
+//                            }
+//                            else{
+                                
+//                            }
+                                
+                            if(matcher.group(2).matches("\\d+")) {  
+                                range_filter_f  =   matcher.group(2);//+";"+matcher.group(3);
+                                range_filter_t  =   matcher.group(3); 
+                                term_filter     =   matcher.group(4);
+                                group           =   "";//matcher.group(5);
+                                sorting         =   "";//matcher.group(6);
+                                order           =   "";//matcher.group(7);
+                                values          =   matcher.group(5);
+                            }
+                            else{
+                                range_filter_f  = matcher.group(5);
+                                range_filter_t  = "";
+                                term_filter     =   matcher.group(3);
+                                group           =   matcher.group(2);
+                                sorting         =   "";//matcher.group(6);
+                                order           =   "";//matcher.group(7);
+                                values          =   matcher.group(4);
+                            }
+//                            
+                            
+                                input   =   "CUBE;"+timestamp+";"+range_filter_f+";"+range_filter_t+";"+term_filter+";"+group+";"+sorting+";"+order+";'"+values+"'";
+                            
+                            System.out.println(input);
+                            output.println(input); 
+                            
+                        }
+                        else{
+                            System.out.println(INPUT_STRING);
+//                             output.println(INPUT_STRING);
+                        }
+                    
+//                    String  edit;    
+//                            
+//                            edit    =	sCurrentLine.replace( ",,,", ";;;")
+//                                            .replace( ",,", ";;")
+//						.replace( "\",\"", ";");
+//                    
+//                    String[] parts          =   edit.split(";");
+//                    
+//                    String  timestamp       =   parts[1]
+//                        ,   range_filter_f  =   parts[2]
+//                        ,   range_filter_t  =   parts[3]
+//                        ,   term_filter     =   parts[4]
+//                        ,   group           =   parts[5]
+//                        ,   sorting         =   parts[7]
+//                        ,   values          =   parts[8]
+//                        ,   input
+//                        ;
+//
+//                    if( (range_filter_f.length()==0) && (range_filter_t.length()==0) ) {
+//                         term_filter  = parts[4];
+////                         HIER MUSS NOCH DAS REGEX FÜR DIE DATEN REIN... DAMIT WENN VON BIN DATUM IN DER VARIABLE IST, 
+////                         DANN NEHM DIE ZU RANGE_FILTER_F AND RANGE_FILTER_T
+//                         
+////                        if(term_filter.matches("*.$[0-9]{8})$.*")) /*[0-9]{8}$.**/
+////                        {
+////                            term_filter  = "CHECK";
+////                        }
+//                    }
+//////                    
+//                    input       =   ("'CUBE';"+timestamp+";"+range_filter_f+";"+range_filter_t+";"+term_filter+";"+group+";"+sorting+";'"+values+"'");
+////                    System.out.println(Arrays.toString(parts));
+////                    System.out.println(term_filter);
 //                    output.println(input);
+                        }
                 }
             }
         catch (IOException e) 
--- src/logparsing_ma/hadoop_parsing.java
+++ src/logparsing_ma/hadoop_parsing.java
@@ -6,19 +6,34 @@
 package logparsing_ma;
 
 import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import static logparsing_ma.Regex.PATTERNS;
 
 /**
  *
  * @author Laura
  */
 class hadoop_parsing {
-        
+        //(USER_ID|TIME_STAMP|WEBSITE_ID|PROG_ID|TCAT_ID)
+    static String[]     REGEXS      = new String[] {    "(.+) INFO .+ (USER_ID|TIME_STAMP|WEBSITE_ID|PROG_ID|TCAT_ID)\\)\\(Version 7\\)\\(Values \\[(\\d+)\\].+TIME_STAMP\\)\\(Version 7\\)\\(LowerTerm (\\d+)\\)\\(IncludeLower true\\)\\(UpperTerm (\\d+).*\\(GroupFields \\[(.+)\\]\\).*\\(ValueFields \\[(.+).*\\(Sort \\[WSort \\(Version 7\\)\\((.+)([ DESC| ASC])\\)\\]\\)\\(.*"
+                                                    ,   "(.+) INFO .+ (USER_ID|TIME_STAMP|WEBSITE_ID|PROG_ID|TCAT_ID)\\)\\(Version 7\\)\\(Values \\[(\\d+)\\].+TIME_STAMP\\)\\(Version 7\\)\\(LowerTerm (\\d+)\\)\\(IncludeLower true\\)\\(UpperTerm (\\d+).*\\(GroupFields \\[(.+)\\]\\).*\\(ValueFields \\[(.+).*\\(Sort \\[WSort \\(Version 7\\)\\]\\)\\(.*"};
+    static Pattern[]    PATTERNS    = new Pattern[REGEXS.length];
+    
+    static {
+        for (int i = 0; i < REGEXS.length; i++){
+            PATTERNS[i] = Pattern.compile(REGEXS[i]);
+        }
+    }
+    
     public static void openHadoop() throws IOException {
 
         String  timestamp      
-            ,   range_filter
+            ,   range_filter_f
+            ,   range_filter_t
             ,   term_filter    
-            ,   group          
+            ,   group  
+            ,   order
             ,   sorting        
             ,   values
             ,   input
@@ -26,78 +41,120 @@ class hadoop_parsing {
             
         
         PrintWriter output     =   new PrintWriter("/Volumes/HD/X/LOGS/FINAL_hadoop.csv");
+        
+        PrintWriter reg_output     =   new PrintWriter("/Volumes/HD/X/LOGS/NOT_REGEX_hadoop.csv");
                 
 //                System.out.println(row);
 
          try (BufferedReader hadoop = new BufferedReader(new FileReader("/Volumes/HD/htdocs/GitHub/log_stuff/log_hadoop")))
             {
-                String sCurrentLine;
+                String  sCurrentLine
+                    ,   INPUT_STRING
+                    ;
     
-                output.println("'System';TimeStamp;RangeFilterFrom;RangeFilterTo;TermFilter;Group;Sorting;Order;Values");
+                output.println("System;TimeStamp;RangeFilterFrom;RangeFilterTo;TermFilter;Group;Sorting;Order;Values");
 
                 while ((sCurrentLine = hadoop.readLine()) != null) {
-          
+                        
                     if((sCurrentLine.contains("WQuery")) ) {
-
-                        timestamp   =   logparsing_methods.getSubString( sCurrentLine, "2", "INFO") ;
                         
-                        timestamp   =   timestamp.replace(" ", "")
-                                            .replace( ",", ":");
+                        INPUT_STRING    =   sCurrentLine;
                         
-                        if(sCurrentLine.contains("[WRangeFilter") && sCurrentLine.contains("e)], [WTermFilter (Field" )) {
-                            
+                        for (Pattern pattern : PATTERNS) {
+                        
+                        Matcher matcher = pattern.matcher(INPUT_STRING);
+                        
+                        if (matcher.matches()){
+                               
+                            if(matcher.groupCount() == 9)  {
+                        // Wenn pattern nicht gefunden, dann sysoutprtln (um neues regex zu bauen)
+                                 timestamp       =   matcher.group(1);
+                                 range_filter_f  =   matcher.group(4);
+                                 range_filter_t  =   matcher.group(5);
+                                 term_filter     =   matcher.group(2) +" : "+ matcher.group(3);
+                                 group           =   matcher.group(6);
+                                 sorting         =   matcher.group(8);
+                                 order           =   matcher.group(9);
+                                 values          =   matcher.group(7);
 
-                            range_filter    =   logparsing_methods.getSubString( sCurrentLine, "[WRangeFilter ", "])") ;
-                            
-                            range_filter    =   range_filter.replace( "[WRangeFilter (Field TIME_STAMP)(Version 7)(LowerTerm ", "")
-                                                    .replace(  ")(IncludeLower true)(UpperTerm", ";")
-                                                        .replace(  ")(IncludeUpper false)]", "")
-                                                            .replace( ", [WTermFilter (Field", ";")
-                                                                .replace(  ")(Version 7)(Values [", " : ")
-                                                    ;
-                            
+                            }
+                            else{ //(matcher.groupCount() == 7){
+                                timestamp       =   matcher.group(1);
+                                range_filter_f  =   matcher.group(4);
+                                range_filter_t  =   matcher.group(5);
+                                term_filter     =   matcher.group(2) +" : "+ matcher.group(3);
+                                group           =   matcher.group(6);
+                                sorting         =   "";
+                                order           =   "";
+                                values          =   matcher.group(7);
+                            }
+                           input   =   "HADOOP;"+timestamp+";"+range_filter_f+";"+range_filter_t+";"+term_filter+";"+group+";"+sorting+";"+order+";"+values+"";
+                                 output.println(input); 
                         }
-                        else 
-                        {	/*Wenn im RangeFIlter kein TermFilter enthalten ist*/	
-                            range_filter    =   logparsing_methods.getSubString( sCurrentLine, "[WRangeFilter "	, "])") ;
-                            
-                            range_filter    =   range_filter.replace( "[WRangeFilter (Field TIME_STAMP)(Version 7)(LowerTerm ", "")
-                                                        .replace(")(IncludeLower true)(UpperTerm", ";")
-                                                            .replace( ")(IncludeUpper false)]", "");
-                            
-                        } 
-//                    HIER IST IRGENDWO NOCH EIN PROBLEM....WEIL MANCHMAL BEIM GROUP NOCH EIN TERMFILTER VORHANDEN IST!!!!!
-                   
-                    term_filter =   logparsing_methods.getSubString( sCurrentLine, "[WTermFilter", ")]" ) ;
-                            
-                    term_filter =   term_filter.replace( ")(Version 7)(Values [", " : ")
-                                        .replace( "WTermFilter (Field ", "") 
-                                            .replace( "[", "")
-                                                .replace( "]", " ");
-                    
-                    group       =   logparsing_methods.getSubString( sCurrentLine,  "(GroupFields"	, "])" );        
-                            
-                    group       =   group.replace( "(GroupFields [", "");
-                    
-                    
-                    
-                    sorting     =   logparsing_methods.getSubString( sCurrentLine,"[WSort " , "])" );
-//                    System.out.println(sorting);
-                    sorting     =   sorting.replace( "[WSort (Version 7)(", "")
-                                            .replace( "DESC", ";DESC")
-                                                .replace( "ASC", ";ASC").replace( ")", "").replace( "[WSort (Version 7", "");
-
-                    values          =   logparsing_methods.getSubString(sCurrentLine, "(ValueFields"	, "])" ) ;
-                    
-                    values          =	values.replace( "(ValueFields [", "");
-                    
-// 				System.out.println(values);								
-                    
-//                    System.out.println("  GROUP"+term_filter);
-                    input   =   "'HADOOP';"+timestamp+";"+range_filter+";"+term_filter+""+group+";"+sorting+";'"+values+"'";
-                    
-                    output.println(input);
-                    
+                        else{
+//                            System.out.println(INPUT_STRING);
+                             reg_output.println(INPUT_STRING);
+                        }
+                        
+//                        timestamp   =   logparsing_methods.getSubString( sCurrentLine, "2", "INFO") ;
+//                        
+//                        timestamp   =   timestamp.replace(" ", "")
+//                                            .replace( ",", ":"); // SimpleDateFormat
+//                        
+//                        if(sCurrentLine.contains("[WRangeFilter") && sCurrentLine.contains("e)], [WTermFilter (Field" )) {
+//                            
+//
+//                            range_filter    =   logparsing_methods.getSubString( sCurrentLine, "[WRangeFilter ", "])") ;
+//                            
+//                            range_filter    =   range_filter.replace( "[WRangeFilter (Field TIME_STAMP)(Version 7)(LowerTerm ", "")
+//                                                    .replace(  ")(IncludeLower true)(UpperTerm", ";")
+//                                                        .replace(  ")(IncludeUpper false)]", "")
+//                                                            .replace( ", [WTermFilter (Field", ";")
+//                                                                .replace(  ")(Version 7)(Values [", " : ")
+//                                                    ;
+//                            
+//                        }
+//                        else 
+//                        {	/*Wenn im RangeFIlter kein TermFilter enthalten ist*/	
+//                            range_filter    =   logparsing_methods.getSubString( sCurrentLine, "[WRangeFilter "	, "])") ;
+//                            
+//                            range_filter    =   range_filter.replace( "[WRangeFilter (Field TIME_STAMP)(Version 7)(LowerTerm ", "")
+//                                                        .replace(")(IncludeLower true)(UpperTerm", ";")
+//                                                            .replace( ")(IncludeUpper false)]", "");
+//                            
+//                        } 
+////                    HIER IST IRGENDWO NOCH EIN PROBLEM....WEIL MANCHMAL BEIM GROUP NOCH EIN TERMFILTER VORHANDEN IST!!!!!
+//                   
+//                    term_filter =   logparsing_methods.getSubString( sCurrentLine, "[WTermFilter", ")]" ) ;
+//                            
+//                    term_filter =   term_filter.replace( ")(Version 7)(Values [", " : ")
+//                                        .replace( "WTermFilter (Field ", "") 
+//                                            .replace( "[", "")
+//                                                .replace( "]", " ");
+//                    
+//                    group       =   logparsing_methods.getSubString( sCurrentLine,  "(GroupFields"	, "])" );        
+//                            
+//                    group       =   group.replace( "(GroupFields [", "");
+//                    
+//                    
+//                    
+//                    sorting     =   logparsing_methods.getSubString( sCurrentLine,"[WSort " , "])" );
+////                    System.out.println(sorting);
+//                    sorting     =   sorting.replace( "[WSort (Version 7)(", "")
+//                                            .replace( "DESC", ";DESC")
+//                                                .replace( "ASC", ";ASC").replace( ")", "").replace( "[WSort (Version 7", "");
+//
+//                    values          =   logparsing_methods.getSubString(sCurrentLine, "(ValueFields"	, "])" ) ;
+//                    
+//                    values          =	values.replace( "(ValueFields [", "");
+//                    
+//// 				System.out.println(values);								
+//                    
+////                    System.out.println("  GROUP"+term_filter);
+//                    input   =   "'HADOOP';"+timestamp+";"+range_filter_f+";"+range_filter_t+";"+term_filter+";"+group+";"+sorting+";"+order+";'"+values+"'";
+//                    
+//                    output.println(input);
+                        }
                     } 
                     
                 }
@@ -109,6 +166,7 @@ class hadoop_parsing {
                 e.printStackTrace();
        
             }    
+         
         output.close();
         System.out.println("Hadoop done!");    
     }
